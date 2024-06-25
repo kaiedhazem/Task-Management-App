e299548509474be206f3c4d8d80aded9ab500dbd
kaiedhazem <gaiedhazem@gmail.com>
2024-06-25 16:01:57 +0200

    add initial script to add some tasks

---
+++ TaskManagementApp/src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO task (title, description, completed) VALUES ('Task 1', 'Description for Task 1', false);
+INSERT INTO task (title, description, completed) VALUES ('Task 2', 'Description for Task 2', true);
+INSERT INTO task (title, description, completed) VALUES ('Task 3', 'Description for Task 3', false);
